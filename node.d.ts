f72e646fdfb701399b24761b456e479e0e62e04c
prawira <robbyprawira3@gmail.com>
2023-10-06 14:18:34 +0700

    🏷️ Add Type For ENV
    Add typescript type for env file

---
+++ node.d.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  APP_URL: z.string(),
+  APP_PORT: z.number(),
+  APP_ENV: z.enum(['production', 'development']),
+  APP_SECRET: z.string(),
+  ACCESS_TOKEN_SECRET: z.string(),
+  REFRESH_TOKEN_SECRET: z.string()
+})
+
+envSchema.parse(process.env)
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envSchema> { }
+  }
+}
+
