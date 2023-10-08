92f82195e73095e98f261a57bda5453e9f4eefb3
prawira <robbyprawira3@gmail.com>
2023-10-08 11:32:13 +0700

    👔 Change Logic To Hash
    Using Bun module to hash password

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,14 +1,8 @@
-import * as argon2 from "argon2"
 import logger from "./logger"
 
 export const hashPassword = async (password: string) => {
   try {
-    const hashedPassword = await argon2.hash(
-      password,
-      {
-        secret: Buffer.from(process.env.APP_SECRET)
-      }
-    )
+    const hashedPassword = await Bun.password.hash(password)
 
     return hashedPassword
   } catch (error) {
@@ -18,7 +12,7 @@ export const hashPassword = async (password: string) => {
 
 export const verifyPassword = async (password: string, hashedPassword: string) => {
   try {
-    const isPasswordMatch = await argon2.verify(hashedPassword, password)
+    const isPasswordMatch = await Bun.password.verify(password, hashedPassword)
 
     return isPasswordMatch
   } catch (error) {
